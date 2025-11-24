c9bf4870b4fb481620da08ae35e59c5b7ff33e87
Jie Li <lijie_12@hotmail.com>
2025-11-24 00:12:46 +0000

    fix the problem

---
+++ test-tags.js
@@ -0,0 +1,9 @@
+// Test file for auto-numbered tags
+// This file is used to test the tag numbering functionality
+
+// TODO-1: First test task
+// TODO-2: Second test task
+// BUG-1: First bug to fix
+
+// TODO: This should be auto-numbered
+// BUG: This should also be auto-numbered
